18ce071860466bef9671ddd827477f536c7655ce
Шевченко Аліна <zaa51742@gmail.com>
2026-05-30 17:15:32 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Lb_20
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHours = new System.Windows.Forms.TextBox();
+            this.txtMinutes = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(173, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введіть години:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(444, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введіть хвилини:";
+            // 
+            // txtHours
+            // 
+            this.txtHours.Location = new System.Drawing.Point(176, 148);
+            this.txtHours.Name = "txtHours";
+            this.txtHours.Size = new System.Drawing.Size(129, 22);
+            this.txtHours.TabIndex = 2;
+            // 
+            // txtMinutes
+            // 
+            this.txtMinutes.Location = new System.Drawing.Point(447, 148);
+            this.txtMinutes.Name = "txtMinutes";
+            this.txtMinutes.Size = new System.Drawing.Size(138, 22);
+            this.txtMinutes.TabIndex = 3;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblResult.Location = new System.Drawing.Point(243, 318);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(268, 20);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "Тут буде результат у секундах\r\n";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(309, 221);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(148, 39);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Розрахувати";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtMinutes);
+            this.Controls.Add(this.txtHours);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHours;
+        private System.Windows.Forms.TextBox txtMinutes;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lb_20
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int hours = int.Parse(txtHours.Text);
+                int minutes = int.Parse(txtMinutes.Text);
+                int totalSeconds = ConvertTimeToSeconds(hours, minutes);
+
+                lblResult.Text = $"Результат: {totalSeconds} сек.";
+                lblResult.ForeColor = Color.Green;
+            }
+            catch (FormatException)
+            {
+                lblResult.Text = "Помилка: будь ласка, введіть лише цілі числа!";
+                lblResult.ForeColor = Color.Red;
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                lblResult.Text = $"Помилка діапазону: {ex.Message}";
+                lblResult.ForeColor = Color.Red;
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text = $"Щось пішло не так: {ex.Message}";
+                lblResult.ForeColor = Color.Red;
+            }
+            finally
+            {
+                System.Diagnostics.Debug.WriteLine("Обробку натискання кнопки завершено.");
+            }
+        }
+        private int ConvertTimeToSeconds(int hours, int minutes)
+        {
+            if (hours < 0 || minutes < 0)
+            {
+                throw new ArgumentOutOfRangeException("Час не може бути від'ємним!");
+            }
+            if (minutes >= 60)
+            {
+                throw new ArgumentOutOfRangeException("Хвилини не можуть бути 60 або більше!");
+            }
+            return (hours * 3600) + (minutes * 60);
+
+        }
+    }
+}
