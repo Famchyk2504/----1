8f628ee26cf22eea18b3a9e59ad366e4013caf2c
Шевченко Аліна <zaa51742@gmail.com>
2026-06-12 02:18:33 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Lb_24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(22, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(367, 264);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(453, 65);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(367, 264);
+            this.panel2.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(890, 65);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(367, 264);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(22, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(366, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Запустити 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(453, 371);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(367, 36);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Запустити 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(890, 371);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(367, 37);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Запустити  3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(315, 500);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(657, 36);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Запустити  всі";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(22, 427);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(367, 40);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Зупинити 1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(453, 428);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(367, 39);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Зупинити 2";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.Location = new System.Drawing.Point(890, 428);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(367, 39);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Зупинити 3";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.Location = new System.Drawing.Point(315, 556);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(657, 40);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "Зупинити всі";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1283, 617);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторна №24";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lb_24
+{
+    public partial class Form1 : Form
+    {
+        Thread thread1;
+        Thread thread2;
+        Thread thread3;
+        public Form1()
+        {
+            InitializeComponent();
+
+            thread1 =
+                new Thread(new ThreadStart(draw_rect));
+
+            thread2 =
+                new Thread(new ThreadStart(draw_eclips));
+
+            thread3 =
+                new Thread(new ThreadStart(Rnd_num));
+        }
+
+        private void draw_rect()
+        {
+            try
+            {
+                Random rnd = new Random();
+
+                Graphics g =
+                    panel1.CreateGraphics();
+
+                while (true)
+                {
+                    Thread.Sleep(40);
+
+                    g.DrawRectangle(
+                        Pens.Pink,
+                        0,
+                        0,
+                        rnd.Next(panel1.Width),
+                        rnd.Next(panel1.Height));
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void draw_eclips()
+        {
+            try
+            {
+                Random rnd = new Random();
+
+                Graphics g =
+                    panel2.CreateGraphics();
+
+                while (true)
+                {
+                    Thread.Sleep(40);
+
+                    g.DrawEllipse(
+                        Pens.Blue,
+                        0,
+                        0,
+                        rnd.Next(panel2.Width),
+                        rnd.Next(panel2.Height));
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Rnd_num()
+        {
+            try
+            {
+                Random rnd = new Random();
+
+                while (true)
+                {
+                    Thread.Sleep(50);
+
+                    richTextBox1.Invoke(
+                        (MethodInvoker)delegate
+                        {
+                            richTextBox1.AppendText(
+                                rnd.Next().ToString()
+                                + Environment.NewLine);
+                        });
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            thread1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            thread2.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            thread3.Start();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (thread1 != null && thread1.IsAlive)
+                    thread1.Suspend();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (thread2 != null && thread2.IsAlive)
+                    thread2.Suspend();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (thread3 != null && thread3.IsAlive)
+                    thread3.Suspend();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (thread1 != null && thread1.IsAlive)
+                    thread1.Suspend();
+
+                if (thread2 != null && thread2.IsAlive)
+                    thread2.Suspend();
+
+                if (thread3 != null && thread3.IsAlive)
+                    thread3.Suspend();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (thread1 != null)
+                {
+                    if (thread1.ThreadState == ThreadState.Suspended)
+                        thread1.Resume();
+
+                    thread1.Abort();
+                }
+
+                if (thread2 != null)
+                {
+                    if (thread2.ThreadState == ThreadState.Suspended)
+                        thread2.Resume();
+
+                    thread2.Abort();
+                }
+
+                if (thread3 != null)
+                {
+                    if (thread3.ThreadState == ThreadState.Suspended)
+                        thread3.Resume();
+
+                    thread3.Abort();
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
