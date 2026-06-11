d4599a279c6a858b03d97f255a3484bd6a4aeec4
Шевченко Аліна <zaa51742@gmail.com>
2026-06-11 22:49:28 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Lab_23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtX0 = new System.Windows.Forms.TextBox();
+            this.txtY0 = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.panelGraph = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(429, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "x0 :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(430, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "y0 :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(633, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "a :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(635, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "b :";
+            // 
+            // txtX0
+            // 
+            this.txtX0.Location = new System.Drawing.Point(488, 43);
+            this.txtX0.Name = "txtX0";
+            this.txtX0.Size = new System.Drawing.Size(72, 22);
+            this.txtX0.TabIndex = 4;
+            // 
+            // txtY0
+            // 
+            this.txtY0.Location = new System.Drawing.Point(488, 95);
+            this.txtY0.Name = "txtY0";
+            this.txtY0.Size = new System.Drawing.Size(71, 22);
+            this.txtY0.TabIndex = 5;
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(693, 43);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(71, 22);
+            this.txtA.TabIndex = 6;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(693, 95);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(72, 22);
+            this.txtB.TabIndex = 7;
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDraw.Location = new System.Drawing.Point(76, 267);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(147, 43);
+            this.btnDraw.TabIndex = 8;
+            this.btnDraw.Text = "Побудувати";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // panelGraph
+            // 
+            this.panelGraph.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelGraph.Location = new System.Drawing.Point(327, 155);
+            this.panelGraph.Name = "panelGraph";
+            this.panelGraph.Size = new System.Drawing.Size(638, 430);
+            this.panelGraph.TabIndex = 9;
+            this.panelGraph.Paint += new System.Windows.Forms.PaintEventHandler(this.panelGraph_Paint);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(21, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(325, 120);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "                               Варіант 24\r\n\r\nх = х0 ·t + a·cos(t)\r\ny = y₀·t + b·s" +
+    "in(t)\r\n\r\nОбласть зміни параметра: t ∈ [-20; 20]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 597);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panelGraph);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.txtY0);
+            this.Controls.Add(this.txtX0);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtX0;
+        private System.Windows.Forms.TextBox txtY0;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Panel panelGraph;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Lab_23
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        double x0;
+        double y0;
+        double a;
+        double b;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            x0 = Convert.ToDouble(txtX0.Text);
+            y0 = Convert.ToDouble(txtY0.Text);
+            a = Convert.ToDouble(txtA.Text);
+            b = Convert.ToDouble(txtB.Text);
+
+            panelGraph.Invalidate();
+        }
+
+        private void panelGraph_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            int W = panelGraph.Width;
+            int H = panelGraph.Height;
+
+            g.Clear(Color.White);
+
+            g.DrawLine(
+                Pens.Black,
+                20,
+                H / 2,
+                W - 20,
+                H / 2);
+
+            g.DrawLine(
+                Pens.Black,
+                W / 2,
+                20,
+                W / 2,
+                H - 20);
+
+            g.DrawString(
+                "X",
+                Font,
+                Brushes.Black,
+                W - 30,
+                H / 2 + 10);
+
+            g.DrawString(
+                "Y",
+                Font,
+                Brushes.Black,
+                W / 2 + 10,
+                20);
+
+            for (int i = 50; i < W; i += 100)
+            {
+                g.DrawString(
+                    (i - W / 2).ToString(),
+                    Font,
+                    Brushes.Black,
+                    i,
+                    H / 2 + 10);
+            }
+
+            for (int i = 50; i < H; i += 100)
+            {
+                g.DrawString(
+                    (H / 2 - i).ToString(),
+                    Font,
+                    Brushes.Black,
+                    W / 2 + 10,
+                    i);
+            }
+
+            for (int i = 50; i < W; i += 50)
+            {
+                g.DrawLine(Pens.Black,
+                    i,
+                    H / 2 - 5,
+                    i,
+                    H / 2 + 5);
+            }
+
+            for (int i = 50; i < H; i += 50)
+            {
+                g.DrawLine(Pens.Black,
+                    W / 2 - 5,
+                    i,
+                    W / 2 + 5,
+                    i);
+            }
+            List<PointF> points = new List<PointF>();
+
+            double xmin = double.MaxValue;
+            double xmax = double.MinValue;
+
+            double ymin = double.MaxValue;
+            double ymax = double.MinValue;
+
+            for (double t = -20; t <= 20; t += 0.05)
+            {
+                double x = x0 * t + a * Math.Cos(t);
+
+                double y = y0 * t + b * Math.Sin(t);
+
+                if (x < xmin) xmin = x;
+                if (x > xmax) xmax = x;
+
+                if (y < ymin) ymin = y;
+                if (y > ymax) ymax = y;
+            }
+           
+            double scaleX =
+                (W - 100) / (xmax - xmin);
+
+            double scaleY =
+                (H - 100) / (ymax - ymin);
+
+            double scale =
+                Math.Min(scaleX, scaleY);
+
+            for (double t = -20; t <= 20; t += 0.05)
+            {
+                double x = x0 * t + a * Math.Cos(t);
+
+                double y = y0 * t + b * Math.Sin(t);
+
+                float sx =
+                    (float)((x - xmin) * scale + 50);
+
+                float sy =
+                    (float)(H - ((y - ymin) * scale + 50));
+
+                points.Add(
+                    new PointF(sx, sy));
+            }
+
+            if (points.Count > 1)
+            {
+                Pen graphPen =
+                    new Pen(Color.Blue, 2);
+
+                g.DrawLines(
+                    graphPen,
+                    points.ToArray());
+            }
+        }
+        
+    }
+}
