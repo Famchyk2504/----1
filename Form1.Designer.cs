60038b2ed5ddbf9f54d1da24e48cc9561e52bb21
Шевченко Аліна <zaa51742@gmail.com>
2026-06-18 20:19:21 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace Lab26
+{
+    partial class Form1
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblVictim = new System.Windows.Forms.Label();
+            this.txtVictim = new System.Windows.Forms.TextBox();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.lblPlace = new System.Windows.Forms.Label();
+            this.txtPlace = new System.Windows.Forms.TextBox();
+            this.lblReason = new System.Windows.Forms.Label();
+            this.txtReason = new System.Windows.Forms.TextBox();
+            this.txtCompany = new System.Windows.Forms.TextBox();
+            this.lblCompany = new System.Windows.Forms.Label();
+            this.lblInspector = new System.Windows.Forms.Label();
+            this.txtInspector = new System.Windows.Forms.TextBox();
+            this.lblTemplate = new System.Windows.Forms.Label();
+            this.cmbTemplates = new System.Windows.Forms.ComboBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.txtReplace = new System.Windows.Forms.TextBox();
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.lblTemplatePath = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblTemplatePath);
+            this.groupBox1.Controls.Add(this.lblCounter);
+            this.groupBox1.Controls.Add(this.txtReplace);
+            this.groupBox1.Controls.Add(this.txtSearch);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnReplace);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.btnPreview);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.btnCreate);
+            this.groupBox1.Controls.Add(this.cmbTemplates);
+            this.groupBox1.Controls.Add(this.lblTemplate);
+            this.groupBox1.Controls.Add(this.txtInspector);
+            this.groupBox1.Controls.Add(this.lblInspector);
+            this.groupBox1.Controls.Add(this.lblCompany);
+            this.groupBox1.Controls.Add(this.txtCompany);
+            this.groupBox1.Controls.Add(this.txtReason);
+            this.groupBox1.Controls.Add(this.lblReason);
+            this.groupBox1.Controls.Add(this.txtPlace);
+            this.groupBox1.Controls.Add(this.lblPlace);
+            this.groupBox1.Controls.Add(this.dtpDate);
+            this.groupBox1.Controls.Add(this.lblDate);
+            this.groupBox1.Controls.Add(this.txtVictim);
+            this.groupBox1.Controls.Add(this.lblVictim);
+            this.groupBox1.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(971, 645);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Дані документа";
+            // 
+            // lblVictim
+            // 
+            this.lblVictim.AutoSize = true;
+            this.lblVictim.Location = new System.Drawing.Point(41, 46);
+            this.lblVictim.Name = "lblVictim";
+            this.lblVictim.Size = new System.Drawing.Size(161, 20);
+            this.lblVictim.TabIndex = 0;
+            this.lblVictim.Text = "ПІБ потерпілого:";
+            // 
+            // txtVictim
+            // 
+            this.txtVictim.Location = new System.Drawing.Point(225, 38);
+            this.txtVictim.Name = "txtVictim";
+            this.txtVictim.Size = new System.Drawing.Size(243, 28);
+            this.txtVictim.TabIndex = 1;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(40, 296);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(106, 20);
+            this.lblDate.TabIndex = 2;
+            this.lblDate.Text = "Дата події:";
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDate.Location = new System.Drawing.Point(264, 290);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(156, 28);
+            this.dtpDate.TabIndex = 3;
+            // 
+            // lblPlace
+            // 
+            this.lblPlace.AutoSize = true;
+            this.lblPlace.Location = new System.Drawing.Point(41, 140);
+            this.lblPlace.Name = "lblPlace";
+            this.lblPlace.Size = new System.Drawing.Size(115, 20);
+            this.lblPlace.TabIndex = 4;
+            this.lblPlace.Text = "Місце події:";
+            // 
+            // txtPlace
+            // 
+            this.txtPlace.Location = new System.Drawing.Point(225, 132);
+            this.txtPlace.Name = "txtPlace";
+            this.txtPlace.Size = new System.Drawing.Size(243, 28);
+            this.txtPlace.TabIndex = 5;
+            // 
+            // lblReason
+            // 
+            this.lblReason.AutoSize = true;
+            this.lblReason.Location = new System.Drawing.Point(41, 243);
+            this.lblReason.Name = "lblReason";
+            this.lblReason.Size = new System.Drawing.Size(94, 20);
+            this.lblReason.TabIndex = 6;
+            this.lblReason.Text = "Причина:";
+            // 
+            // txtReason
+            // 
+            this.txtReason.Location = new System.Drawing.Point(225, 235);
+            this.txtReason.Multiline = true;
+            this.txtReason.Name = "txtReason";
+            this.txtReason.Size = new System.Drawing.Size(243, 28);
+            this.txtReason.TabIndex = 7;
+            // 
+            // txtCompany
+            // 
+            this.txtCompany.Location = new System.Drawing.Point(225, 183);
+            this.txtCompany.Name = "txtCompany";
+            this.txtCompany.Size = new System.Drawing.Size(243, 28);
+            this.txtCompany.TabIndex = 8;
+            // 
+            // lblCompany
+            // 
+            this.lblCompany.AutoSize = true;
+            this.lblCompany.Location = new System.Drawing.Point(41, 191);
+            this.lblCompany.Name = "lblCompany";
+            this.lblCompany.Size = new System.Drawing.Size(137, 20);
+            this.lblCompany.TabIndex = 9;
+            this.lblCompany.Text = "Підприємство:";
+            // 
+            // lblInspector
+            // 
+            this.lblInspector.AutoSize = true;
+            this.lblInspector.Location = new System.Drawing.Point(41, 95);
+            this.lblInspector.Name = "lblInspector";
+            this.lblInspector.Size = new System.Drawing.Size(105, 20);
+            this.lblInspector.TabIndex = 10;
+            this.lblInspector.Text = "Інспектор:";
+            // 
+            // txtInspector
+            // 
+            this.txtInspector.Location = new System.Drawing.Point(225, 87);
+            this.txtInspector.Name = "txtInspector";
+            this.txtInspector.Size = new System.Drawing.Size(243, 28);
+            this.txtInspector.TabIndex = 11;
+            // 
+            // lblTemplate
+            // 
+            this.lblTemplate.AutoSize = true;
+            this.lblTemplate.Location = new System.Drawing.Point(41, 351);
+            this.lblTemplate.Name = "lblTemplate";
+            this.lblTemplate.Size = new System.Drawing.Size(87, 20);
+            this.lblTemplate.TabIndex = 12;
+            this.lblTemplate.Text = "Шаблон:";
+            this.lblTemplate.Click += new System.EventHandler(this.lblTemplate_Click);
+            // 
+            // cmbTemplates
+            // 
+            this.cmbTemplates.FormattingEnabled = true;
+            this.cmbTemplates.Location = new System.Drawing.Point(225, 343);
+            this.cmbTemplates.Name = "cmbTemplates";
+            this.cmbTemplates.Size = new System.Drawing.Size(243, 28);
+            this.cmbTemplates.TabIndex = 13;
+            this.cmbTemplates.SelectedIndexChanged += new System.EventHandler(this.cmbTemplates_SelectedIndexChanged);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(644, 38);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(285, 54);
+            this.btnCreate.TabIndex = 14;
+            this.btnCreate.Text = "Створити документ";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(644, 280);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(285, 53);
+            this.btnSave.TabIndex = 15;
+            this.btnSave.Text = "Зберегти документ";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Location = new System.Drawing.Point(644, 199);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(285, 53);
+            this.btnPreview.TabIndex = 16;
+            this.btnPreview.Text = "Перегляд шаблону";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(644, 119);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(285, 53);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "Очистити форму";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 458);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 20);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Змінити на:";
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(225, 500);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(243, 42);
+            this.btnReplace.TabIndex = 20;
+            this.btnReplace.Text = "Замінити";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 403);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 20);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Пошук:";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(225, 395);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(243, 28);
+            this.txtSearch.TabIndex = 22;
+            // 
+            // txtReplace
+            // 
+            this.txtReplace.Location = new System.Drawing.Point(225, 450);
+            this.txtReplace.Name = "txtReplace";
+            this.txtReplace.Size = new System.Drawing.Size(243, 28);
+            this.txtReplace.TabIndex = 23;
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.AutoSize = true;
+            this.lblCounter.Location = new System.Drawing.Point(688, 366);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(219, 20);
+            this.lblCounter.TabIndex = 24;
+            this.lblCounter.Text = "Створено документів: 0";
+            // 
+            // lblTemplatePath
+            // 
+            this.lblTemplatePath.AutoSize = true;
+            this.lblTemplatePath.Location = new System.Drawing.Point(44, 580);
+            this.lblTemplatePath.Name = "lblTemplatePath";
+            this.lblTemplatePath.Size = new System.Drawing.Size(168, 20);
+            this.lblTemplatePath.TabIndex = 25;
+            this.lblTemplatePath.Text = "Шлях до шаблону";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 669);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Генератор документа \"Нещасний випадок\"";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.TextBox txtVictim;
+        private System.Windows.Forms.Label lblVictim;
+        private System.Windows.Forms.TextBox txtReason;
+        private System.Windows.Forms.Label lblReason;
+        private System.Windows.Forms.TextBox txtPlace;
+        private System.Windows.Forms.Label lblPlace;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.ComboBox cmbTemplates;
+        private System.Windows.Forms.Label lblTemplate;
+        private System.Windows.Forms.TextBox txtInspector;
+        private System.Windows.Forms.Label lblInspector;
+        private System.Windows.Forms.Label lblCompany;
+        private System.Windows.Forms.TextBox txtCompany;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtReplace;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCounter;
+        private System.Windows.Forms.Label lblTemplatePath;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace Lab26
+{
+    public partial class Form1 : Form
+    {
+        private Word.Application wordApp;
+        private Word.Document wordDoc;
+
+        private string[] templates;
+        private int documentsCreated = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string templateFolder =
+    Path.Combine(
+        Application.StartupPath,
+        "Templates");
+
+                templates =
+                    Directory.GetFiles(
+                        templateFolder,
+                        "*.dotx");
+
+                cmbTemplates.Items.Clear();
+
+                foreach (string file in templates)
+                {
+                    cmbTemplates.Items.Add(
+                        Path.GetFileNameWithoutExtension(file));
+                }
+
+                if (cmbTemplates.Items.Count > 0)
+                    cmbTemplates.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Помилка завантаження шаблонів:\n" +
+                    ex.Message);
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(txtVictim.Text) ||
+                 string.IsNullOrWhiteSpace(txtPlace.Text) ||
+                string.IsNullOrWhiteSpace(txtReason.Text) ||
+               string.IsNullOrWhiteSpace(txtCompany.Text) ||
+               string.IsNullOrWhiteSpace(txtInspector.Text))
+            {
+                MessageBox.Show(
+                    "Заповніть усі поля!");
+
+                return;
+            }
+
+            try
+            {
+                if (cmbTemplates.SelectedIndex < 0)
+                {
+                    MessageBox.Show(
+                        "Оберіть шаблон!");
+
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(txtVictim.Text))
+                {
+                    MessageBox.Show(
+                        "Введіть ПІБ потерпілого!");
+
+                    return;
+                }
+
+                string template =
+                    templates[cmbTemplates.SelectedIndex];
+
+                wordApp = new Word.Application();
+
+                wordDoc =
+                    wordApp.Documents.Add(template);
+
+                FillContentControl(
+                    "Victim",
+                    txtVictim.Text);
+
+                FillContentControl(
+                    "Date",
+                    dtpDate.Value.ToShortDateString());
+
+                FillContentControl(
+                    "Place",
+                    txtPlace.Text);
+
+                FillContentControl(
+                    "Reason",
+                    txtReason.Text);
+
+                FillContentControl(
+                    "Company",
+                    txtCompany.Text);
+
+                FillContentControl(
+                    "Inspector",
+                    txtInspector.Text);
+
+                documentsCreated++;
+
+                lblCounter.Text =
+                    "Створено документів: " +
+                    documentsCreated;
+
+                wordApp.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Помилка створення документа:\n" +
+                    ex.Message);
+            }
+        }
+
+        private void FillContentControl(
+            string title,
+            string value)
+        {
+            foreach (Word.ContentControl cc
+                in wordDoc.ContentControls)
+            {
+                if (cc.Title == title)
+                {
+                    cc.Range.Text = value;
+                }
+            }
+        }
+
+        private void btnPreview_Click(
+            object sender,
+            EventArgs e)
+        {
+            try
+            {
+                if (cmbTemplates.SelectedIndex < 0)
+                {
+                    MessageBox.Show(
+                        "Оберіть шаблон!");
+
+                    return;
+                }
+
+                Process.Start(
+                    templates[cmbTemplates.SelectedIndex]);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSave_Click(
+            object sender,
+            EventArgs e)
+        {
+            if (wordDoc == null)
+            {
+                MessageBox.Show(
+                    "Спочатку створіть документ!");
+
+                return;
+            }
+
+            SaveFileDialog sfd =
+                new SaveFileDialog();
+
+            sfd.Filter =
+                "Word document (*.docx)|*.docx";
+
+            if (sfd.ShowDialog() ==
+                DialogResult.OK)
+            {
+                wordDoc.SaveAs2(
+                    sfd.FileName);
+
+                MessageBox.Show(
+                    "Документ успішно збережено!");
+            }
+        }
+
+        private void Form1_FormClosed(
+            object sender,
+            FormClosedEventArgs e)
+        {
+            try
+            {
+                if (wordDoc != null)
+                    wordDoc.Close(false);
+
+                if (wordApp != null)
+                    wordApp.Quit();
+            }
+            catch
+            {
+            }
+        }
+
+        private void lblTemplate_Click(
+            object sender,
+            EventArgs e)
+        {
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtVictim.Clear();
+            txtPlace.Clear();
+            txtReason.Clear();
+            txtCompany.Clear();
+            txtInspector.Clear();
+            txtSearch.Clear();
+            txtReplace.Clear();
+
+            dtpDate.Value = DateTime.Now;
+
+            cmbTemplates.SelectedIndex = 0;
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            if (wordDoc == null)
+            {
+                MessageBox.Show("Документ не створено!");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtSearch.Text))
+            {
+                MessageBox.Show("Введіть текст для пошуку!");
+                return;
+            }
+
+            Word.Range range = wordDoc.Content;
+
+            Word.Find find = range.Find;
+
+            find.ClearFormatting();
+            find.Replacement.ClearFormatting();
+
+            bool result = find.Execute(
+                FindText: txtSearch.Text,
+                ReplaceWith: txtReplace.Text,
+                Replace: Word.WdReplace.wdReplaceAll);
+
+            if (result)
+                MessageBox.Show("Заміну виконано.");
+            else
+                MessageBox.Show("Текст для заміни не знайдено.");
+        }
+
+        private void cmbTemplates_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbTemplates.SelectedIndex >= 0)
+            {
+                lblTemplatePath.Text =
+                    templates[
+                        cmbTemplates.SelectedIndex];
+            }
+        }
+    }
+}
